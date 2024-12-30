8d551aa7d70c5d1b1e61dc47bc5faa976381b409
SuleimanKh97 <suleiman97kh@gmail.com>
2024-12-30 20:48:12 +0300

    taskday2v1.00

---
+++ day1/index.js
@@ -0,0 +1,8 @@
+
+const myH2 = document.getElementById("myh2");
+myH2.textContent = "hello, world";
+var greeter = "hey hi";
+
+
+console.log(greeter);
+alert("this is an alert");
---
+++ day2-30-12-24/1.js
@@ -0,0 +1,18 @@
+// Object:
+const person = {firstName:"Salem", lastName:"Mohammed"};
+
+// console.log(person.firstName , typeof person.firstName);
+// console.log(person.lastName);
+
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+let list = [4, 5, 6];
+
+for (let i in list) {
+   console.log(i); // "0", "1", "2",
+}
+
+
+
